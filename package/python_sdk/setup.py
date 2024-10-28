f31030bfa2ba3ef2e81a17f585a9b1ddb0e75e20
MakerYang <open@wileho.com>
2024-10-28 15:53:46 +0800

    update python sdk

--- package/python_sdk/setup.py
+++ package/python_sdk/setup.py
@@ -12,33 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-import shutil
 from setuptools import setup, find_packages
-
-dist_directory = "dist"
-if os.path.exists(dist_directory):
-    shutil.rmtree(dist_directory)
-    shutil.rmtree("build")
-    shutil.rmtree("armcnc.egg-info")
-
-with open("readme.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setup(
     name="armcnc",
-    version="1.1.26",
+    version="2.0.0",
     author="MakerYang",
     author_email="admin@wileho.com",
     description="Python development framework for armcnc.",
-    long_description=long_description,
+    long_description="",
     long_description_content_type="text/markdown",
-    url="https://github.com/armcnc/python",
+    url="https://github.com/armcnc/armcnc",
     packages=find_packages(),
     classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Multimedia :: Sound/Audio",
+        "Topic :: Multimedia :: Video",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    python_requires=">=3.7.5",
+    python_requires=">=3.9.0",
+    license="Apache-2.0",
+    project_urls={
+        "Documentation": "https://www.armcnc.net",
+        "Website": "https://armcnc.net",
+        "Source": "https://github.com/armcnc/armcnc",
+    },
+    keywords=["armcnc", "cnc", "arm64", "arm", "linuxcnc", "ubuntu"],
+    install_requires=["requests>=2.32.3"]
 )
