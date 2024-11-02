929e08316ac91b9cc72e51d65b2a1362f9f3ec1c
MakerYang <open@wileho.com>
2024-11-02 11:19:23 +0800

    update services

--- backend/service/handler/index.go
+++ backend/service/handler/index.go
@@ -15,6 +15,7 @@
 package handler
 
 import (
+	"github.com/armcnc/armcnc/backend/package/display"
 	"github.com/armcnc/armcnc/backend/package/version"
 	"github.com/armcnc/armcnc/backend/utils"
 	"github.com/gin-gonic/gin"
@@ -34,7 +35,7 @@ func Index(c *gin.Context) {
 	returnData.Version = version.Get.Version
 	returnData.Describe = version.Get.Describe
 
-	// display.Get.Switch("on")
+	display.Get.Switch("on")
 
 	utils.Success(c, returnData)
 	return
