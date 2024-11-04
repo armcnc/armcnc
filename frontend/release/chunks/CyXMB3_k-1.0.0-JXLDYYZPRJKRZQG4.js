835b2937c558535cbf2e3e5d8cf171bc22eb547b
MakerYang <open@wileho.com>
2024-11-04 16:29:50 +0800

    update frontend

--- frontend/release/chunks/CyXMB3_k-1.0.0-JXLDYYZPRJKRZQG4.js
+++ frontend/release/chunks/CyXMB3_k-1.0.0-JXLDYYZPRJKRZQG4.js
@@ -1,9 +1,9 @@
-import{h as s}from"./CpqZPhky-1.0.0-K4255IB8AN4ZGUCX.js";/**
+import{h}from"./CpqZPhky-1.0.0-JXLDYYZPRJKRZQG4.js";/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const k=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
+ */const y=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase();/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -13,12 +13,12 @@ import{h as s}from"./CpqZPhky-1.0.0-K4255IB8AN4ZGUCX.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const w=({size:e,strokeWidth:a=2,absoluteStrokeWidth:n,color:c,iconNode:r,name:d,class:y,...i},{slots:h})=>s("svg",{...o,width:e||o.width,height:e||o.height,stroke:c||o.stroke,"stroke-width":n?Number(a)*24/Number(e):a,class:["lucide",`lucide-${k(d??"icon")}`],...i},[...r.map(l=>s(...l)),...h.default?[h.default()]:[]]);/**
+ */const l=({size:e,strokeWidth:a=2,absoluteStrokeWidth:n,color:c,iconNode:r,name:d,class:p,...i},{slots:s})=>h("svg",{...o,width:e||o.width,height:e||o.height,stroke:c||o.stroke,"stroke-width":n?Number(a)*24/Number(e):a,class:["lucide",`lucide-${y(d??"icon")}`],...i},[...r.map(k=>h(...k)),...s.default?[s.default()]:[]]);/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const t=(e,a)=>(n,{slots:c})=>s(w,{...n,iconNode:a,name:e},c);/**
+ */const t=(e,a)=>(n,{slots:c})=>h(l,{...n,iconNode:a,name:e},c);/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -28,7 +28,7 @@ import{h as s}from"./CpqZPhky-1.0.0-K4255IB8AN4ZGUCX.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const g=t("OctagonMinusIcon",[["path",{d:"M2.586 16.726A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2h6.624a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586z",key:"2d38gg"}],["path",{d:"M8 12h8",key:"1wcyev"}]]);/**
+ */const f=t("OctagonMinusIcon",[["path",{d:"M2.586 16.726A2 2 0 0 1 2 15.312V8.688a2 2 0 0 1 .586-1.414l4.688-4.688A2 2 0 0 1 8.688 2h6.624a2 2 0 0 1 1.414.586l4.688 4.688A2 2 0 0 1 22 8.688v6.624a2 2 0 0 1-.586 1.414l-4.688 4.688a2 2 0 0 1-1.414.586H8.688a2 2 0 0 1-1.414-.586z",key:"2d38gg"}],["path",{d:"M8 12h8",key:"1wcyev"}]]);/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
@@ -38,9 +38,14 @@ import{h as s}from"./CpqZPhky-1.0.0-K4255IB8AN4ZGUCX.js";/**
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const f=t("PlayIcon",[["polygon",{points:"6 3 20 12 6 21 6 3",key:"1oa8hb"}]]);/**
+ */const g=t("PlayIcon",[["polygon",{points:"6 3 20 12 6 21 6 3",key:"1oa8hb"}]]);/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
  * See the LICENSE file in the root directory of this source tree.
- */const m=t("PowerIcon",[["path",{d:"M12 2v10",key:"mnfbl"}],["path",{d:"M18.4 6.6a9 9 0 1 1-12.77.04",key:"obofu9"}]]);export{u as M,g as O,f as P,M as a,m as b};
+ */const b=t("PowerOffIcon",[["path",{d:"M18.36 6.64A9 9 0 0 1 20.77 15",key:"dxknvb"}],["path",{d:"M6.16 6.16a9 9 0 1 0 12.68 12.68",key:"1x7qb5"}],["path",{d:"M12 2v4",key:"3427ic"}],["path",{d:"m2 2 20 20",key:"1ooewy"}]]);/**
+ * @license lucide-vue-next v0.438.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=t("PowerIcon",[["path",{d:"M12 2v10",key:"mnfbl"}],["path",{d:"M18.4 6.6a9 9 0 1 1-12.77.04",key:"obofu9"}]]);export{u as M,f as O,g as P,M as a,b,m as c};
--- frontend/src/pages/common/header.vue
+++ frontend/src/pages/common/header.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="w-full h-[65px] flex flex-row items-center">
         <div class="w-full">1</div>
-        <div class="w-full flex flex-row justify-center space-x-2">
+        <div class="w-full flex flex-row justify-center space-x-3">
             <div class="w-auto">
-                <Button class="text-muted-foreground hover:bg-primary/0 focus:bg-primary/0 focus-visible:bg-primary/0" variant="outline" size="sm">
-                    <PlayIcon class="w-3 h-3" />
+                <Button class="text-muted-foreground hover:bg-primary/0 focus:bg-primary/0 focus-visible:bg-primary/0" variant="outline" size="default">
+                    <PlayIcon class="w-4 h-4" />
                 </Button>
             </div>
             <div class="w-auto">
@@ -13,21 +13,21 @@
                 </Button>
             </div>
             <div class="w-auto">
-                <Button class="text-muted-foreground" variant="outline" size="sm">
-                    <OctagonMinusIcon class="w-3 h-3" />
+                <Button class="text-muted-foreground" variant="outline" size="default">
+                    <OctagonMinusIcon class="w-4 h-4" />
                 </Button>
             </div>
         </div>
         <div class="w-full flex flex-row justify-end space-x-2 pr-2">
             <div class="w-auto">
-                <Button class="text-muted-foreground hover:bg-primary/0 focus:bg-primary/0 focus-visible:bg-primary/0" variant="outline" size="sm">
-                    <PowerIcon class="w-3 h-3 mr-2" />
+                <Button class="text-white hover:bg-primary/0 focus:bg-primary/0 focus-visible:bg-primary/0" variant="destructive" size="sm">
+                    <PowerOffIcon class="w-3 h-3 mr-2" />
                     <span>急停</span>
                 </Button>
             </div>
             <div class="w-auto">
                 <Button class="text-muted-foreground" variant="outline" size="default">
-                    <PlayIcon class="w-4 h-4 mr-2" />
+                    <PowerIcon class="w-4 h-4 mr-2" />
                     <span>启动</span>
                 </Button>
             </div>
@@ -44,7 +44,7 @@
 <script setup lang="ts">
 import {nextTick, onBeforeMount, onBeforeUnmount, onMounted, onUnmounted} from "vue";
 import {Button} from "../../packages/york";
-import {MapPinOffIcon, PlayIcon, PowerIcon, PauseIcon, OctagonMinusIcon} from "lucide-vue-next";
+import {MapPinOffIcon, PlayIcon, PowerIcon, PowerOffIcon, PauseIcon, OctagonMinusIcon} from "lucide-vue-next";
 
 const props: any = defineProps<{
     data: any
