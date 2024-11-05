1806310f413fb833781279e0270ae99b42d27db1
MakerYang <open@wileho.com>
2024-11-05 16:28:02 +0800

    update frontend

--- frontend/release/chunks/o6PjB4CT-1.0.0-KN9HQV66SBF4N40E.js
+++ frontend/release/chunks/o6PjB4CT-1.0.0-KN9HQV66SBF4N40E.js
@@ -1,4 +1,4 @@
-import{h as c}from"./DerUGcCz-1.0.0-BZYOCY42Y5R4BGPW.js";/**
+import{h as c}from"./DerUGcCz-1.0.0-KN9HQV66SBF4N40E.js";/**
  * @license lucide-vue-next v0.438.0 - ISC
  *
  * This source code is licensed under the ISC license.
