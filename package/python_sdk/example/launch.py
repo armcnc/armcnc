bf19bc6698e1ea5378938c45cf2fd840abe162c4
MakerYang <open@wileho.com>
2024-11-10 15:08:50 +0800

    update python sdk

--- package/python_sdk/example/launch.py
+++ package/python_sdk/example/launch.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 import armcnc
 
 def armcnc_start(sdk):
-    print(2)
+    print(f"2")
     sdk.utils.log.ignore("armcnc_start")
     while not sdk.quit_event.is_set():
         pass
