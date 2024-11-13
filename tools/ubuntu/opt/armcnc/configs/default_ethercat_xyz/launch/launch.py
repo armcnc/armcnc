758a2f07c3620370fc3fd883b1f02f459e78fa8d
MakerYang <open@wileho.com>
2024-11-13 16:38:57 +0800

    update configs launchs

--- tools/ubuntu/opt/armcnc/configs/default_ethercat_xyz/launch/launch.py
+++ tools/ubuntu/opt/armcnc/configs/default_ethercat_xyz/launch/launch.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import armcnc
+sys.path.append("/usr/lib/python3/dist-packages")
 
 def armcnc_start(sdk):
     sdk.utils.log.ignore("armcnc_start")
